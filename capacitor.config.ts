68bb79ada607c8f13c7456e0183f7863c2c50b78
phanbao1408-cmyk <phanbao1408@gmail.com>
2026-04-12 12:09:09 +0700

    Update fix loi Supabase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,15 @@
-/// <reference types="@capacitor/local-notifications" />
-
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vlu.digiwell',
   appName: 'DigiWell',
   webDir: 'dist',
+  bundledWebRuntime: false,
   plugins: {
-    LocalNotifications: {
-      iconColor: '#06B6D4',
-    },
-  },
+    CapacitorHttp: {
+      enabled: true,
+    }
+  }
 };
 
 export default config;
